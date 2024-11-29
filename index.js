d089b705cf4e2b70619f66b68f79eed7506b5703
nilayganatra027@gmail.com <nilayganatra027@gmail.com>
2024-11-29 18:49:22 -0500

    initail commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+// Middleware for parsing JSON
+app.use(express.json());
+
+// Static endpoint 1: Home
+app.get('/', (req, res) => {
+    res.send('Welcome to the Node.js ES6 App!');
+});
+
+// Static endpoint 2: About
+app.get('/about', (req, res) => {
+    res.json({
+        message: 'This is a simple Node.js application.',
+        developer: 'Rachana Ganatra',
+    });
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
